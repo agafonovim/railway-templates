dc6154bff93f3be0dc93617c5d789b020f51005e
Andrey Agafonov <andrey@agafonov.im>
2026-08-24 13:03:57 +0200

    fix(flipt): add envoy healthz route; render listener port from PORT

--- flipt/envoy/render-config.sh
+++ flipt/envoy/render-config.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 set -eu
 
+: "${PORT:=8080}"
 : "${FLIPT_UPSTREAM_HOST:=flipt.railway.internal}"
 : "${FLIPT_UPSTREAM_PORT:=9000}"
 
-sed -e "s|@FLIPT_UPSTREAM_HOST@|${FLIPT_UPSTREAM_HOST}|g" \
+sed -e "s|@PORT@|${PORT}|g" \
+    -e "s|@FLIPT_UPSTREAM_HOST@|${FLIPT_UPSTREAM_HOST}|g" \
     -e "s|@FLIPT_UPSTREAM_PORT@|${FLIPT_UPSTREAM_PORT}|g" \
     /etc/envoy/envoy.yaml.tmpl > /etc/envoy/envoy.yaml
 
